3ff2f8c82529d42b31cc4560da73b01f2831f97f
Mykyta Serhiienko <mykyta.serhiienko@globallogic.com>
2022-06-02 16:15:25 +0300

    Add decorator

---
+++ design_patterns/structural/decorator.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+def make_blink(function):
+    """Defines a decorator"""
+
+    # This makes the decorator transparent in terms of its name and docstring
+    @wraps(function)
+
+    # Defines the inner function
+    def decorator():
+        # Grab the return value of the function being decorated
+        ret = function()
+
+        # Add new functionality to the function being decorated
+        return f"<blink>{ret}</blink>"
+    return decorator
+
+# Apply the decorator
+@make_blink
+def hello():
+    """Original function"""
+    return "Hello"
+
+# Check the result of decorating
+print(hello())
+
+# Check that the function name is the original
+print(hello.__name__)
+
+# Check that the docstring is from the original function
+print(hello.__doc__)
+
